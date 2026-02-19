1c269f9c1da080a5b21a6fec0aa4b77269444aba
Tanvi Suryawanshi <144896856+Tanvi2505@users.noreply.github.com>
2026-02-19 16:47:47 +0530

    Add files via upload

---
+++ Buzz.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Buzz{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num=sc.nextInt();
+		if(num%7==0||num%10==7)
+		{
+			System.out.println("It is buzz number");
+		}
+		else{
+			System.out.println("It is not Buzz number");
+		}
+	}
+}

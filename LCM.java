894bb2014552f6d6e127e62ebadd618a4ae24780
Tanvi Suryawanshi <144896856+Tanvi2505@users.noreply.github.com>
2026-04-05 14:54:13 +0530

    Add files via upload

---
+++ LCM.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+class LCM {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a first number:");
+        int n1 = sc.nextInt();
+
+        System.out.println("Enter a second number:");
+        int n2 = sc.nextInt();
+
+        int large = n1 > n2 ? n1 : n2;
+        int i = 1;
+
+        while (true) {
+            if ((large * i) % n1 == 0 && (large * i) % n2 == 0) 
+            {
+                System.out.println(large*i);
+                break;
+            }
+            i++;
+        }
+    }
+}

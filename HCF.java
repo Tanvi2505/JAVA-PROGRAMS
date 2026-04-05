58b30e70b879aa2a3c6b0d1fccf1651342589231
Tanvi Suryawanshi <144896856+Tanvi2505@users.noreply.github.com>
2026-04-05 15:00:20 +0530

    Add files via upload

---
+++ HCF.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class HCF{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter a first number:");
+        int n1 = sc.nextInt();
+
+        System.out.println("Enter a second number:");
+        int n2 = sc.nextInt();
+
+        int small = n1<n2 ? n1:n2;
+         while(true)
+         {
+         	if(n1 % small ==0 && n2 % small ==0)
+         	{
+         		System.out.println(small);
+         		break;
+
+         	}
+         	small--;
+         }
+	}
+}

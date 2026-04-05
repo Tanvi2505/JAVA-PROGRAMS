c1fdb0482b1d0a9eca81cb2db20c72282e5d55ea
Tanvi Suryawanshi <144896856+Tanvi2505@users.noreply.github.com>
2026-04-05 15:09:51 +0530

    Add files via upload

---
+++ Ugly.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Ugly{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number:");
+		int num=sc.nextInt();
+		while(num!=0)
+		{
+			if(num%2==0)
+			{
+				num=num/2;
+			}
+			else if(num%3==0)
+			{
+				num=num/3;
+			}
+			else if(num%5==0)
+			{
+				num=num/5;
+			}
+			else{
+				break;
+				}
+	}
+			if(num==1)
+			{
+				System.out.println("It is Ugly Number");
+			}
+			else{
+				System.out.println("It is Not Ugly Number");
+			}
+		}
+	}
